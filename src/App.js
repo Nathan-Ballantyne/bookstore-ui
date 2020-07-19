84042fc9e70d22abc5eeff789b40d1b96ee3ed41
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-07-19 12:36:36 +1200

    rafactor narbar items out and add components

--- src/App.js
+++ src/App.js
@@ -1,47 +1,42 @@
 import React from 'react';
 import Sidebar from './components/Sidebar/Sidebar';
+import TopPanel from './components/MainPanel/TopPanel';
+import Dashboard from './components/Dashboard/Dashboard';
+import styled from 'styled-components';
+import items from './components/Sidebar/Items';
 
-const items = [
-  { name: 'home', label: 'Home' },
-  {
-    name: 'billing',
-    label: 'Billing',
-    items: [
-      { name: 'statements', label: 'Statements' },
-      { name: 'reports', label: 'Reports' },
-    ],
-  },
-  {
-    name: 'settings',
-    label: 'Settings',
-    items: [
-      { name: 'profile', label: 'Profile' },
-      { name: 'insurance', label: 'Insurance' },
-      {
-        name: 'notifications',
-        label: 'Notifications',
-        items: [
-          { name: 'email', label: 'Email' },
-          {
-            name: 'desktop',
-            label: 'Desktop',
-            items: [
-              { name: 'schedule', label: 'Schedule' },
-              { name: 'frequency', label: 'Frequency' },
-            ],
-          },
-          { name: 'sms', label: 'SMS' },
-        ],
-      },
-    ],
-  },
-]
+const Background = styled.div`
+    background-image: url('https://images.unsplash.com/photo-1594794031248-c58cbc04fd13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80');
+    position: absolute;
+    z-index: 0;
+    height: 100%;
+    width: 100%;
+    display: block;
+    top: 0;
+    left: 0;
+    background-size: cover;
+    background-position: center center;
+    &::after {
+      position: absolute;
+      z-index: 3;
+      width: 100%;
+      height: 100%;
+      content: "";
+      display: block;
+      background: black;
+      opacity: 0.6;
+    }
+`;
+
 
 function App() {
   return (
-    <div>
+    <>
+      <Background/>
       <Sidebar items={items} />
-    </div>
+      <TopPanel />
+      <Dashboard/>
+    </>
   )
 }
 
