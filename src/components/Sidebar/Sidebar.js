8c9fa1dd573941dd96c5fbee050da3938b9f7d7b
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-07-19 15:56:16 +1200

    clean up unsed props

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,14 +1,8 @@
 import React from "react";
 import List from "@material-ui/core/List";
 import Divider from "@material-ui/core/Divider";
-import styled from "styled-components";
 import SidebarItem from './SidebarItem';
 
-const Div = styled.div`
-  max-width: 240px;
-  border: 1px solid rgba(0, 0, 0, 0.1);
-  height: 100%;
-`;
 
 function Sidebar({ items, depthStep, depth, expanded }) {
   return (
