e987d62c4ef3d0cc2709dc2fc7b17f6211cfca2f
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-07-19 15:21:31 +1200

    add SmallCardBar compenent to hold small cards

---
+++ src/components/SmallCard/SmallCardBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+import SmallCard from './SmallCard';
+import bookIcon from '../../assets/images/book-icon.png';
+import swordIcon from '../../assets/images/sword-icon.png';
+
+const Bar = styled.div`
+    margin-top: 30px;
+    margin-left: 10px;
+    z-index: 1;
+    width: 100%
+`;
+
+function SmallCardBar() {
+    return (
+        <Bar>
+            <SmallCard title="Books" details="List of all books" iconPath={bookIcon}/>
+           
+            <SmallCard title="Fantasy" details="List of all Fantasy books" iconPath={swordIcon}/>
+        </Bar>
+    );
+}
+
+export default SmallCardBar;
