c9477ab03d0728c983e00086ba345acdde71fe64
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-07-19 15:21:04 +1200

    add SmallCard component

---
+++ src/components/SmallCard/SmallCard.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Card = styled.div`
+    background-color: #FFFFFF;
+    height: 150px;
+    width: 350px;
+    font-size: 25px;
+    color: grey;
+    z-index: 1;
+    border-radius: 25px;
+    text-align: center;
+    position: relative;
+    margin-left: 20px;
+    box-shadow: 3px 3px 3px 3px grey; 
+    float:left;
+    transition: top ease 0.5s;
+    &:hover{
+        top: -6px;
+        left: -2px;
+        cursor: pointer;
+    }
+`;
+
+const Detail = styled.p`
+    color: grey;
+    font-family: Roboto,Helvetica,Arial,sans-serif;
+    text-align: left;
+    padding-left: 20px;
+    padding-top: 15px;
+    font-size: 15px;
+`;
+
+const Icon = styled.img`
+    margin-top: 30px;
+    margin-left: 10px;
+    box-shadow: 3px 3px 3px 3px #EEEEEE; 
+    border-radius: 30px;
+    height: 32px;
+    width: 32px;
+`;
+
+
+function SmallCard({title = "title", details = "details", iconPath}) {
+    Icon.defaultProps = {
+        src: iconPath,
+    };
+    return (
+        <Card>
+            {title}
+            <Icon></Icon>
+            <Detail>
+                {details}
+            </Detail>
+        </Card>
+    );
+}
+
+export default SmallCard;
