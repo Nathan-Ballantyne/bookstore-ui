a3fa044fcc9dc51ddd29e55fedc9e98b75a30a3b
Nathan <nathan.ballantyne@gmail.com>
2020-07-20 18:58:52 +1200

    change style and add search bar

--- src/components/MainPanel/TopPanel.js
+++ src/components/MainPanel/TopPanel.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import SearchBar from './SearchBar';
 
 const Main = styled.div`
     display: inline-block;
     margin-left: 240px;
     color:  #818181;
     font-size: 30px;
-    background-color: black;
+    background-color: #EEEEEE;
     height: 100px;
     position: absolute;
     width: 100%;
-    opacity: 0.9;
-    text-align: center;
+    text-align: left;
+    padding-left: 30px;
     text-decoration: none;
     z-index: 1;
 `;
@@ -33,6 +34,7 @@ const topPanel = () => {
             <PanelText>
                 Bookstore
             </PanelText>
+            <SearchBar />
         </Main>
     );
 }
