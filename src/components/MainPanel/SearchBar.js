0770fa7299c4e07b337624f9cb8efc596ae9873d
Nathan <nathan.ballantyne@gmail.com>
2020-07-20 19:38:52 +1200

    remove border add shadow

--- src/components/MainPanel/SearchBar.js
+++ src/components/MainPanel/SearchBar.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 
 const Input = styled.input`
     border-radius: 25px;
+    border: none;
+    box-shadow: 3px 3px 3px 3px grey; 
     height: 25px;
     width: 200px;
     margin-bottom: 10px;
