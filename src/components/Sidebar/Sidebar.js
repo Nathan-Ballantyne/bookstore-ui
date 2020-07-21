dca8a0302e15e03e5901cb09eb1ed01271aacda4
Nathan <nathan.ballantyne@gmail.com>
2020-07-21 18:59:14 +1200

    change sidebar container div to use styled-components

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,11 +2,25 @@ import React from "react";
 import List from "@material-ui/core/List";
 import Divider from "@material-ui/core/Divider";
 import SidebarItem from './SidebarItem';
+import styled from 'styled-components';
+
+
+const Sidebar = styled.div` 
+  width: 240px;
+  height: 100vh;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  background-color: black;
+  opacity: 0.8;
+  z-index: 1;
+  overflow-x: hidden;
+  position: fixed;
+`;
+
 
 
 const sidebar = ({ items, depthStep, depth, expanded }) => {
   return (
-    <div className="sidebar">
+    <Sidebar>
       <List disablePadding dense>
         {items.map((sidebarItem, index) => (
           <React.Fragment key={`${sidebarItem.name}${index}`}>
@@ -23,7 +37,7 @@ const sidebar = ({ items, depthStep, depth, expanded }) => {
           </React.Fragment>
         ))}
       </List>
-    </div>
+    </Sidebar>
   );
 }
 
