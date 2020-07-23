c1e0191005db81bdcde2e20e277a09ab3a3cc473
Nathan <nathan.ballantyne@gmail.com>
2020-07-23 21:02:58 +1200

    use book component

--- src/components/Pages/AllBooks/AllBooks.js
+++ src/components/Pages/AllBooks/AllBooks.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Book from '../Book';
 
 const AllBooksBoard = styled.div`
     margin-left: 240px;
@@ -14,9 +15,34 @@ const AllBooksBoard = styled.div`
     position: absolute;
 `;
 
+
+
+const book = {
+    id: 1,
+    title: 'The Name of the Wind',
+    author: 'Patrick Rothfuss',
+    releaseYear: '2008',
+    pageCount: 672,
+    cover: 'location',
+    series: 'Kingkiller Chronicle',
+    readStatus: 'read',
+    rating: 10,
+};
+
 const AllBooksPage = () => {
     return (
-        <AllBooksBoard />
+        <AllBooksBoard>
+            <Book id={book.id}
+                title={book.title}
+                author={book.author}
+                releaseYear={book.releaseYear}
+                pageCount={book.pageCount}
+                cover={book.cover}
+                series={book.series}
+                readStatus={book.readStatus}
+                rating={book.rating}
+            />
+        </AllBooksBoard>
     );
 };
 
