fbf610d09a8fabf08ff36eca72ff6fe05af3883c
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-07-25 11:39:31 +1200

    add dark mode

--- src/components/MainPanel/SearchBar.js
+++ src/components/MainPanel/SearchBar.js
@@ -2,14 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Input = styled.input`
+    background-color: ${props => props.dark ? '#29303B' : '#FFFFFF'};
+    color: ${props => props.dark ? 'white' : 'grey'};
     border-radius: 25px;
     border: none;
-    box-shadow: 3px 3px 3px 3px grey; 
+    box-shadow: 3px 3px 3px 3px ${props => props.dark ? 'black' : 'grey'}; 
     height: 25px;
     width: 200px;
     margin-bottom: 10px;
     margin-left: 250px;
     text-align: center;
+    &::placeholder {
+        color: ${props => props.dark ? 'white' : 'grey'};
+    }
 `;
 
 Input.defaultProps = {
@@ -17,9 +22,9 @@ Input.defaultProps = {
     placeholder: 'Search'
 }
 
-const searchBar = () => {
+const searchBar = ({dark = false}) => {
     return (
-        <Input />
+        <Input dark={dark} />
     );
 };
 
