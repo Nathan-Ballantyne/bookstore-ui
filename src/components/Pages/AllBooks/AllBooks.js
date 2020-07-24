73020b9a5af5d4c7f7dfbb76e89455c6149dc12f
Nathan <nathan@zealsystems.co.nz>
2020-07-24 12:56:48 +1200

    restructure book.js file

--- src/components/Pages/AllBooks/AllBooks.js
+++ src/components/Pages/AllBooks/AllBooks.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Book from '../Book';
+import Book from '../../Books/Book';
 import { Dashboard } from '../Dashboard/Dashboard';
 
 
