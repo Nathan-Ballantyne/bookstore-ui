c72f11738a10e262478978c3e5912382874369f8
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2020-07-26 11:23:28 +1200

    display books from api request

--- src/components/Pages/AllBooks/AllBooks.js
+++ src/components/Pages/AllBooks/AllBooks.js
@@ -1,60 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //import styled from 'styled-components';
-import Book from '../../Books/Book';
-import { Dashboard } from '../Dashboard/Dashboard';
+import Book from "../../Books/Book";
+import { Dashboard } from "../Dashboard/Dashboard";
 
+// const book = {
+//   id: 1,
+//   title: "The Name of the Wind",
+//   author: "Patrick Rothfuss",
+//   releaseYear: "2008",
+//   pageCount: 672,
+//   cover: "location",
+//   series: "Kingkiller Chronicle",
+//   readStatus: "read",
+//   rating: 10,
+// };
 
-
-const book = {
-    id: 1,
-    title: 'The Name of the Wind',
-    author: 'Patrick Rothfuss',
-    releaseYear: '2008',
-    pageCount: 672,
-    cover: 'location',
-    series: 'Kingkiller Chronicle',
-    readStatus: 'read',
-    rating: 10,
-};
-
-const fetchAllBooks = async () => {
-    const response = await fetch('/allBooks');
-    const body = await response.json();
-    return body;
-  };
-
-//   <Book id={book.id}
-//                 title={book.title}
-//                 author={book.author}
-//                 releaseYear={book.releaseYear}
-//                 pageCount={book.pageCount}
-//                 cover={book.cover}
-//                 series={book.series}
-//                 readStatus={book.readStatus}
-//                 rating={book.rating}
-//                 dark={dark}
-//             />
+// const fetchAllBooks = async () => {
+//   const response = await fetch("/allBooks");
+//   const body = await response.json();
+//   return body;
+// };
 
 class AllBooksPage extends Component {
+  constructor(props) {
+    super(props);
+    this.dark = props.dark;
+    this.state = {
+      bookRes: [],
+    };
+  }
 
-    constructor({ dark }){
-        super(dark);
-        this.dark = dark;
-    }
+  componentDidMount() {
+    fetch("/api/allBooks")
+      .then((res) => res.json())
+      .then((res) => this.setState({ bookRes: res }))
+      .catch(console.log('call to /api/allBooks failed'));
+  }
 
-    componentDidMount() {
-        fetch('/api/allBooks')
-          .then(res => res.json())
-          .then(console.log);
-      }
-
-    render() {
-        return (
-            <Dashboard dark={this.dark}>
-            </Dashboard>
-        );
-    }
-    
-};
+  render() {
+    return (
+      <Dashboard dark={this.dark}>
+        {this.state.bookRes.map((book) => {
+          return (
+            <Book
+              id={book.id}
+              title={book.title}
+              author={book.author}
+              releaseYear={book.releaseYear}
+              pageCount={book.pageCount}
+              cover={book.cover}
+              series={book.series}
+              readStatus={book.readStatus}
+              rating={book.rating}
+              dark={this.props.dark}
+              key={book.id}
+            />
+          );
+        })}
+      </Dashboard>
+    );
+  }
+}
 
 export default AllBooksPage;
