dea67cdc0d86764479bf1918ebe83f5cbf24f23b
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-07-25 16:07:03 +1200

    comment unused import

--- src/components/Pages/AllBooks/AllBooks.js
+++ src/components/Pages/AllBooks/AllBooks.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+//import styled from 'styled-components';
 import Book from '../../Books/Book';
 import { Dashboard } from '../Dashboard/Dashboard';
 
